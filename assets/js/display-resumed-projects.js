a012b68ab77a92a3e15c5b5b9e2b2a604c722e49
Deiluis <luisconforti04@gmail.com>
2025-08-23 14:49:38 -0300

    Modified projects and services sections

--- assets/js/display-resumed-projects.js
+++ assets/js/display-resumed-projects.js
@@ -1,45 +1,61 @@
 import { resumed } from "./projects.js";
 
 const generateProjects = () => {
-    const projectsSection = document.querySelector("#proyectos");
+    const projectsContainer = document.querySelector(".proyectos__container");
 
-    console.log(projectsSection);
+    resumed.forEach((project, i) => {
 
-    resumed.forEach(project => {
         // Crear el artículo
         const article = document.createElement('article');
-        article.className = "card h-[30rem] text-white relative overflow-hidden rounded transition-shadow duration-500 hover:shadow-2xl hover:shadow-black";
-        
+        article.className = "project lg:h-[85vh] bg-white transition-shadow duration-300 shadow-2xl shadow-[--black] rounded-xl";
+        article.setAttribute("data-aos", `fade-${i % 2 == 0 ? "right" : "left"}`);
+
         // Crear el enlace dentro del artículo
         const a = document.createElement('a');
         a.href = project.link;
-        a.className = "bg-[#2227] opacity-100 md:opacity-0 hover:opacity-100 transition-opacity duration-500 absolute bottom-0 left-0 z-[1000] w-full h-full flex flex-col items-center justify-center";
+        a.className = `
+            project__link w-full h-full flex flex-col-reverse items-center
+            ${i % 2 == 0 ? 'lg:flex-row' : 'lg:flex-row-reverse'}
+        `;
         
+        const sideText = document.createElement('div');
+        sideText.className = "lg:w-1/4 lg:h-full lg:p-16 p-8 flex flex-col lg:gap-20 gap-8 items-center justify-center";
+
         // Crear el título y la descripción dentro del enlace
         const h3 = document.createElement('h3');
-        h3.className = "text-6xl";
+        h3.className = "text-4xl"
         h3.textContent = project.title;
         
+        const p = document.createElement('p');
+        p.className = "leading-[1.5]";
+        p.textContent = project.description;
+
         const span = document.createElement('span');
-        span.className = "text-3xl mt-8";
+        span.className = "transition-color duration-300 text-[--light-gray]";
         span.innerHTML = `Ver el proyecto <i class="fa-solid fa-chevron-right"></i>`;
         
         // Crear la imagen dentro del artículo
         const img = document.createElement('img');
-        img.className = "w-full h-full object-cover transition-transform duration-500";
+        img.className = `
+            lg:w-3/4 lg:h-full w-full h-96 object-cover rounded-t-xl
+            ${i % 2 == 0 ? 'lg:rounded-r-xl lg:rounded-tl-none' : 'lg:rounded-l-xl lg:rounded-tr-none'}
+        `;
         img.src = project.img;
         img.alt = `Imagen del proyecto ${project.title}`;
         
-        // Agregar el título y la descripción al enlace
-        a.appendChild(h3);
-        a.appendChild(span);
+        sideText.appendChild(h3);
+        sideText.appendChild(p);
+        sideText.appendChild(span);
         
-        // Agregar el enlace y la imagen al artículo
+        a.appendChild(sideText);
+        a.appendChild(img);
+        
+        // Agregar el enlace con todo al articulo.
         article.appendChild(a);
-        article.appendChild(img);
+        
         
         // Agregar el artículo a la sección
-        projectsSection.appendChild(article);
+        projectsContainer.appendChild(article);
     });
 }
 
