8a77497a94790638742336f92a292b7eedd1a736
Deiluis <luisconforti04@gmail.com>
2026-03-20 13:30:17 -0300

    Fixed header size and visibility issues. Resized og image

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,10 +1,7 @@
-const header = document.querySelector('#header');
 const navOpenBtn = document.querySelector('.nav__btn--open');
 const navCloseBtn = document.querySelector('.nav__btn--close');
 const navList = document.querySelector('.nav__list');
 const navLinks = document.querySelectorAll('.nav__list a');
-
-let inHero = true;
 
 const toggleNavBar = () => {
     navOpenBtn.classList.toggle('rotate-90');
@@ -19,32 +16,6 @@ const toggleNavBar = () => {
 navOpenBtn.addEventListener("click", toggleNavBar);
 navCloseBtn.addEventListener("click", toggleNavBar);
 
-navLinks.forEach((navLink) => {
-    navLink.addEventListener("click", toggleNavBar);
-});
-
-window.addEventListener("scroll", () => {
-    if (inHero) {
-        if (window.scrollY >= window.screen.availHeight) {
-            inHero = false;
-            header.classList.toggle("bg-transparent");
-            header.classList.toggle("backdrop-blur-lg");
-            header.classList.toggle("bg-white");
-
-            navList.classList.toggle("bg-transparent");
-            navList.classList.toggle("backdrop-blur-lg");
-            navList.classList.toggle("bg-white");
-        }
-    } else {
-        if (window.scrollY < window.screen.availHeight) {
-            inHero = true;
-            header.classList.toggle("bg-transparent");
-            header.classList.toggle("backdrop-blur-lg");
-            header.classList.toggle("bg-white");
-
-            navList.classList.toggle("bg-transparent");
-            navList.classList.toggle("backdrop-blur-lg");
-            navList.classList.toggle("bg-white");
-        }
-    }
-})
+navLinks.forEach(navLink =>
+    navLink.addEventListener("click", toggleNavBar)
+);
